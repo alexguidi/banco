9812b6b8bd63faa6834658678c786be15e7dd422
Alex Guidi <aguidi@tfd.ie>
2020-10-15 17:11:21 +0100

    Pointers

--- main.go
+++ main.go
@@ -10,8 +10,49 @@ type CheckingAccount struct {
 }
 
 func main() {
+	alexAccount := CheckingAccount{}
+	alexAccount.holder = "Alex"
+	alexAccount.balance = 500
+
+	fmt.Println(alexAccount.balance)
+
+	fmt.Println(alexAccount.withdraw(-100))
+
+	fmt.Println(alexAccount.balance)
+}
+
+func (c *CheckingAccount) withdraw(withdrawValue float64) string {
+	canWithdraw := withdrawValue > 0 && withdrawValue <= c.balance
+
+	if canWithdraw {
+		c.balance -= withdrawValue
+		return "Withdraw performed successfully"
+	} else {
+		return "There is no balance available to withdraw"
+	}
+}
+
+func comparations() {
 	alexAccount := CheckingAccount{holder: "Alex", agencyNumber: 589, accountNumber: 123456, balance: 125.5}
+	alexAccount2 := CheckingAccount{holder: "Alex", agencyNumber: 589, accountNumber: 123456, balance: 125.5}
+
+	fmt.Println(alexAccount == alexAccount2)
+
 	nycolleAccount := CheckingAccount{"Nycolle", 222, 111222, 200.0}
-	fmt.Println(alexAccount)
 	fmt.Println(nycolleAccount)
+
+	var betoAccount *CheckingAccount
+	betoAccount = new(CheckingAccount)
+	betoAccount.holder = "Beto"
+	betoAccount.balance = 300.5
+
+	var betoAccount2 *CheckingAccount
+	betoAccount2 = new(CheckingAccount)
+	betoAccount2.holder = "Beto"
+	betoAccount2.balance = 300.5
+
+	fmt.Println(&betoAccount)
+	fmt.Println(&betoAccount2)
+	fmt.Println(betoAccount2 == betoAccount)
+	fmt.Println(*betoAccount == *betoAccount2)
 }
